7e1a29895e509aff6f73121b134bfb77ff913f6b
Jackson <jttrainer@dmacc.edu>
2021-10-06 19:19:41 -0500

    inital project creation.

---
+++ Forever_Home_Finder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Forever_Home_Finder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
